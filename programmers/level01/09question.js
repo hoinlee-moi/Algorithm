234ae16f975db65cce4dc44fab63b7f876578a60
hoinlee-moi <lhi3048@gmail.com>
2022-10-10 16:37:35 +0900

    modify : 주석 처리되었던 것 복구

--- programmers/level01/09question.js
+++ programmers/level01/09question.js
@@ -1,14 +1,14 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120813
 
-// function solution(n) {
-//     let answer = [];
-//     for(let i=1; i<=n; i++) {
-//         if(i%2 !=0) {
-//             answer.push(i)
-//         }
-//     }
-//     return answer;
-// }
+function solution(n) {
+    let answer = [];
+    for(let i=1; i<=n; i++) {
+        if(i%2 !=0) {
+            answer.push(i)
+        }
+    }
+    return answer;
+}
 
 // for문으로 n이하의 정수를 하나하나 꺼내 나눗셈 나머지(%)를 이용 홀짝 구분
 
