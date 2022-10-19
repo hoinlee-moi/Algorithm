255d2726cff14e6dc2cfafd27a7618b2bf7f2dfb
hoinlee-moi <lhi3048@gmail.com>
2022-10-20 00:17:46 +0900

    add : 알고리즘 문제 추가

--- programmers/level00/23question.js
+++ programmers/level00/23question.js
@@ -27,3 +27,47 @@ function solution(s) {
 그 값을 배열로 집어놓도록 만든것이다.
 그러면 그 값이 하나라면 처음부터 찾아서 발견한 인덱스와 끝에서부터 발견한 인덱스가 같은 경우를 구하는 것.
 */
+
+
+
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120890#
+
+
+function solution(array, n) {
+    array.sort((a,b)=>a-b)
+    const subArr = array.map(v=>Math.abs(n-v));
+    const minIndex = subArr.indexOf(Math.min(...subArr))
+    return array[minIndex];
+}
+/*
+먼저 작은 수를 반환해야 하기 때문에 찾기 쉽도록 오름차순 정렬을 한뒤 abs로 주어진 n과의 거리 값을 구한 동일한 길이의 배열을 준비한다.
+이후 가장 작은 값의 첫번째를 순서를 반환하는 indexOf를 이용해 가장 작은 값 즉 거리가 가장 가까운 값의 순서를 구해 array에 집어넣어 완료한다.
+다른 사람 풀이도 많이 있지만 무조건 한줄이라고 좋은 건 아닌 것 같았다. 짧긴하지만 가로로 길었고 그때문에 가독성 부분에서 매우 읽기 힘들었다.
+사용한 문법은 같았다.
+*/
+
+
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120838
+
+
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+};
+    return letter.split(' ').map(l => morse[l]).join('');;
+}
+
+/*
+처음엔 하드코딩 했는데 이게 객체 배열을사용할 줄 몰라 일어난 것 같다. 이렇게 map을 이용해 나눈 배열에서 찾아나가면 되는데 굉장히 오래 고민했다. 반성하자
+*/
+
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120848
+
+
