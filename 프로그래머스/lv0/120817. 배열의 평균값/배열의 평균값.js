0f6ba2fc8e63e8e3b1bec880e407b9c96b8adf69
hoinlee-moi <103922658+hoinlee-moi@users.noreply.github.com>
2022-10-29 23:59:47 +0900

    [level 0] Title: 배열의 평균값, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -3,3 +3,8 @@ function solution(numbers) {
     const answer = sum/numbers.length
     return answer;
 }
+
+/*
+먼저 배열의 합을 array함수인 reduce를 이용하여 더함 이후 평균값을 길이로 나눠 값을 return한다.
+이 때 제한사항에 정수만 return하란 게 없으니 그대로 
+*/
