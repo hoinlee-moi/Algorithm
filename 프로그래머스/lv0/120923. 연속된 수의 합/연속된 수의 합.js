933b6738ff67959f97ad898968d6373acd2cde19
hoinlee-moi <103922658+hoinlee-moi@users.noreply.github.com>
2022-10-31 21:29:35 +0900

    Update 연속된 수의 합.js

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -11,3 +11,19 @@ function solution(num, total) {
     }
     return answer;
 }
+
+/*
+수학 공식을 이용했는데 연속된 숫자의 개수가 짝수면 total을 2 나눈 값이 총 숫자의 개수/2 만큼 등장해야 total이 나온다. 이를 이용해 for문으로 숫자를 삽입
+홀수라면 연속된 숫자의 개수로 total을 나눈 값이 연속된 숫자의 가운데 값이 되고 이를 이용해 (연속된 숫자개수-1)/2 값이 양쪽으로 연속될 숫자의 개수이다. 이를 이용해 for문으로 삽입
+*/
+
+function solution(num, total) {
+    var min = Math.ceil(total/num - Math.floor(num/2));
+    var max = Math.floor(total/num + Math.floor(num/2));
+
+    return new Array(max-min+1).fill(0).map((el,i)=>{return i+min;});
+}
+
+/*
+다른 사람 풀이 -  연속될 숫자의 최소와 최대를 구한 후 배열의 길이를 설정한 뒤 fill로 채우고 map을 이용해 숫자를 입력한다.
+*/
