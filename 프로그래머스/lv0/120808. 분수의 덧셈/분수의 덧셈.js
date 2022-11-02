c8f1ada4a9fd79c24873b1dce7cca57a8f7e9638
hoinlee-moi <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-02 23:19:00 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,20 @@
+function solution(denum1, num1, denum2, num2) {
+    let answer = []
+    const numerator = (denum1*num2)+(denum2*num1)
+    const denominator = num1*num2
+    const maxMeasure = measure(numerator,denominator);
+    answer.push(numerator/maxMeasure)
+    answer.push(denominator/maxMeasure)
+    return answer
+}
+
+
+
+const measure = (num1,num2) => {
+    let measure = []
+    const maxNum = Math.max(num1,num2)
+    for(let i=1; i<=maxNum; i++) {
+        if(num1%i===0 && num2%i===0) measure.push(i)
+    }
+    return Math.max(...measure)
+}
