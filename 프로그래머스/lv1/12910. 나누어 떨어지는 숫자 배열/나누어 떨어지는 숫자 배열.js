f92650dca479d3f822d9f0add69699aceb9e5835
hoinlee-moi <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-09 20:16:09 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -1,6 +1,15 @@
 function solution(arr, divisor) {
+    const answer = arr.filter(v=> v%divisor===0);
+    answer.sort((a,b)=>a-b)
+    return answer.length?answer:[-1];
+}
+
+/*
+아래는 이전 풀이인데 filter를 사용하면 되는데 각 계산을 전부 한다고 생각하여 map을 사용하게 됐다.
+function solution(arr, divisor) {
     var answer = [];
     arr.map(v=> {if(v%divisor===0)  answer.push(v)})
     answer.sort((a,b)=>a-b)
     return answer.length?answer:[-1];
 }
+*/
