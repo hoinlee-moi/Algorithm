228490bcf6e27286fe38c4b9cf102583a1949274
hoinlee-moi <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-10 18:40:53 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -5,3 +5,12 @@ function solution(absolutes, signs) {
     },0);
     return answer;
 }
+
+/*
+for문이 reduce보다 성능이 좋음으로 reduce를 사용하는 것도 좋을 것 같다.
+let sum = 0
+absoulutes.forEach((el,i)=> {
+signs[i]===true ? sum += el : sum -= el
+})
+return sum
+*/
