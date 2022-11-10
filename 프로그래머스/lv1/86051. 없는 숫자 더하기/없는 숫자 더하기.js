6e64f6743a7eb8597974d6c11688db1105ec06c0
hoinlee-moi <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-10 18:49:29 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -5,3 +5,11 @@ function solution(numbers) {
     }
     return answer;
 }
+
+/*
+다른 사람 풀이를 보니까 바로 알아차린게 1~9까지의 모든 숫자의 합에서 배열에 있는 합을 빼버리면
+배열에 없는 숫자들의 합이 도출되긴 한다!
+function solution(numbers) {
+    return 45 - numbers.reduce((acc,cur ) => acc + cur, 0);
+}
+*/
