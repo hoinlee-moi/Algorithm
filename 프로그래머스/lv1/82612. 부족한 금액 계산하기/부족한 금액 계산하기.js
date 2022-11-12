fa999b55129f18d5dd34aa628804c7c9cd92c775
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-12 22:06:18 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -5,3 +5,13 @@ function solution(price, money, count) {
     if(cost>money) return cost-money
     else if(cost<=money) return 0
 }
+
+
+/*
+수학공식을 이용하면 더 쉽게 풀리는 게 있었다.
+function solution(price, money, count) {
+    const tmp = price * count * (count + 1) / 2 - money;
+    return tmp > 0 ? tmp : 0;
+}
+1 ~ x까지의 합 n * (n + 1) / 2인데 여기에 price배 만큼 곱해주는 거니까 (앞의 식) * price
+*/
