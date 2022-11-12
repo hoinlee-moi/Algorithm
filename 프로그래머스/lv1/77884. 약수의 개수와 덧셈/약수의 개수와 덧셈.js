249f0b9a5962927b89649643253f71cf7688439b
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-12 20:09:11 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.74 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -13,3 +13,20 @@ const measure = num => {
     }
     return measure.length%2===0?true:false
 }
+
+/*
+나같은 경우 되게 하드코딩을 했는데 다른 사람 풀이를 보고 놀랐다.
+개념적인 부분인데 제곱근이 정수면 약수의 개수가 홀수를 주는 건데 
+function solution(left, right) {
+    var answer = 0;
+    for (let i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
+    }
+    return answer;
+}
+로 해결됐다
+*/
