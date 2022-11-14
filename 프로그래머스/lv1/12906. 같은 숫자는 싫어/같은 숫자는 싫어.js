4ac031aeecc2c8b01bd1c909edb38fefaa6f0ca4
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-14 11:05:38 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 22.77 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,3 +1,8 @@
 function solution(arr) {
     return arr.filter((v,i,ar)=> v!=ar[i+1]);
 }
+
+/*
+처음엔 map으로 사용해 봤는데 생각해보면 filter또한 각 원소마다 주어진 조건이 일치한지 확인하고 걸러주는 것이기에
+사용할 수 있다고 판단, 사용해 보니 값이 도출 되었습니다.
+*/
