1cc0ff156f08025984e81f8a9c1dc4b216175289
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-14 12:31:33 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,3 +1,9 @@
 function solution(n) {
     return Number.parseInt(n.toString(3).split("").reverse().join(""),3);
 }
+
+/*
+내장함수인 parseInt을 사용했습니다.
+n을 먼저 toString으로 3진법으로 변환 후 배열화(split)=>뒤집기(reverse) 후 join으로 다시 돌린 후 
+pasreInt를 사용하여 다시 10진법으로 변환 하였습니다.
+*/
