df23100c359f9a6af9a6f6b8f4f08b3f91190a95
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-15 21:48:41 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,4 +1,18 @@
 function solution(d, budget) {
+    d.sort((a,b)=>a-b)
+    let result = 0;
+    let answer = 0;
+    for(let i=0; i<d.length; i++) {
+    result += d[i]
+    if(result>budget) return answer
+    answer++    
+    }
+    return answer
+}
+
+/*
+그런데 이것보다 빠른 방법이 확실히 있다. reduce를 사용해 봤지만 for문을 이용하는 것이 break문을 통해 불필요한 작업을 줄일 수 있다.
+function solution(d, budget) {
     let result = 0
     d.sort((a,b)=>a-b)
     const answer = d.reduce((acc,cur)=>{
@@ -10,3 +24,4 @@ function solution(d, budget) {
     },0);
     return result;
 }
+*/
