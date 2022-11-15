f7cf5dc8fb2d47d64d9137d7fbd9b8aee8becf40
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-15 21:52:12 +0900

    Update 예산.js

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -11,7 +11,8 @@ function solution(d, budget) {
 }
 
 /*
-그런데 이것보다 빠른 방법이 확실히 있다. reduce를 사용해 봤지만 for문을 이용하는 것이 break문을 통해 불필요한 작업을 줄일 수 있다.
+처음엔 아래 있는 reduce식을 사용해 봤지만 for문을 이용하는 것이 break문을 통해 불필요한 작업을 줄일 수 있다고 생각했다.
+또한 for문 자체도 속도가 더 빠르기 때문에 차이가 있으리라 생각
 function solution(d, budget) {
     let result = 0
     d.sort((a,b)=>a-b)
@@ -24,4 +25,5 @@ function solution(d, budget) {
     },0);
     return result;
 }
+결과 for문이 조금 더 빠르다 reduce는 정답이 나왔음에도 계속해서 돌아간 후 답을 도출해내는 반면 for는 답이 도출 되면 끝나서 그런 것 같다.
 */
