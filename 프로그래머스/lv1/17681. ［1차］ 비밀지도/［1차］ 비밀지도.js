306e48a22b644436cf698e06d976f52533c4e40e
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-16 15:50:39 +0900

    Update ［1차］ 비밀지도.js

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,17 +1,17 @@
-// function solution(n, arr1, arr2) {
-//     const secretMap1 = arr1.map(v=>v.toString(2).padStart(n,'0'))
-//     const secretMap2 = arr2.map(v=>v.toString(2).padStart(n,'0'))
-//     const answer = secretMap1.map((v,i) => {
-//         let password = ""
-//         for(let j=0; j<n; j++) { 
-//             if(v[j]==='0' && secretMap2[i][j]==='0') password+=" "
-//             else password += "#"
-//         }
-//         return password
-//     })
-//     return answer;
-// }
-let solution=(n,a,b)=>a.map((a,i)=>(a|b[i]).toString(2).padStart(n,0).replace(/0/g,' ').replace(/1/g,'#'))
+function solution(n, arr1, arr2) {
+    const secretMap1 = arr1.map(v=>v.toString(2).padStart(n,'0'))
+    const secretMap2 = arr2.map(v=>v.toString(2).padStart(n,'0'))
+    const answer = secretMap1.map((v,i) => {
+        let password = ""
+        for(let j=0; j<n; j++) { 
+            if(v[j]==='0' && secretMap2[i][j]==='0') password+=" "
+            else password += "#"
+        }
+        return password
+    })
+    return answer;
+}
+
 
 
 /*
