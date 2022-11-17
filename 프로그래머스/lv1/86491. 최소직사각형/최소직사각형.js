35cb80cf1bd4837e55a2a92109912452cf5de497
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-17 12:27:15 +0900

    [level 1] Title: 최소직사각형, Time: 1.49 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -3,3 +3,9 @@ function solution(sizes) {
     let vertical = sizes.map(v=> v[0]>v[1]?v[1]:v[0])
     return Math.max(...width)*Math.max(...vertical)
 }
+
+/*
+먼저 가로에서 가장 크게 들어갈 길이값을 필요하기에 카드를 회전해도 가장 큰 값을 도출해내도록 만들었다.
+map을 이용해 크기 비교로 각 카드들의 가로세로 구분없이 가장 긴 값과 나머지값들을 구해
+가장 긴 값과 나머지 값들중 가장 긴 값을 곱하여 답을 도출
+*/
