9d559d7a99bce63b277869bfb8b73a4a01c9028a
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-22 00:14:24 +0900

    Update 모의고사.js

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -18,6 +18,11 @@ function solution(answers) {
     return answer.map((v,i)=>Math.max(...answer)===v? i+1 : null).filter(v=>v!=null)
 }
 /*
+많이 바보같이 풀긴 했다. 계산하기 위해서 반복문이 총 4번 돌아갔는데 repeat까지 포함하니 훨씬 더 많았다.
+순서공식을 생각 안한게 한이다.. 정말
+일다 내 풀이는 각각의 패턴들이 모두 똑같이 순환하는 값을 구해서 repeat으로 다 붙인 다음 splice로 정답배열(answers)을 제거해가며 풀었다.
+최소로 겹치는 수가 40개라 40개씩 잘라내서 비교했는데 더 좋은 방법이 아래 있었다.
+
 function solution(answers) {
     var answer = [];
     const man1 = [1, 2, 3, 4, 5];
@@ -38,4 +43,7 @@ function solution(answers) {
 
     return answer;
 }
+먼저 반복되는 패턴을 배열로 둔다(이 부분은 나도 처음에 시도를 했다.) 그 후 각 반복되는 길이만큼 i를 나눈 나머지값을 도출하면
+그 반복되는 패턴의 순번으로 비교할 수 있다.
+그걸 생각 못한 게 너무 아쉬운 것 같다.
 */
