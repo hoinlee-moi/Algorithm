42a263cc0210061b97d7d6e1e31e4bf7295c7838
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-21 19:35:37 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,4 @@
 function solution(nums) {
-    const set = new Set(nums)
-    const newNums = [...set]
-    if(newNums.length<nums.length/2) return newNums.length
-    else if(newNums.length>=nums.length/2) return nums.length/2
+    var answer = 0;
+    return answer;
 }
