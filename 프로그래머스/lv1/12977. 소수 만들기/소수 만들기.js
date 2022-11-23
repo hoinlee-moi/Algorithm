a3bfb841c0153ec1f33cb55ca4f4c029e51b9e4d
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-23 15:07:30 +0900

    Update 소수 만들기.js

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -12,8 +12,12 @@ function solution(nums) {
 
 const isPrime = num => {
     if(num===2) return true
-        for(let i=2; i<=Math.ceil(Math.sqrt(num)); i++) {
+        for(let i=2; i<=Math.sqrt(num); i++) {
             if(num%i===0) return false
         }
     return true
 }
+
+/*
+먼저 소수 판별 함수를 따로 만든 후 3중첩 반복문을 이용해 각 경우의 수를 이용해 배열의 3자리 수의 합을 구하도록 했다.
+*/
