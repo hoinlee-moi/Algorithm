ee81f23092cf750e3bc4b66c311918c497ae7153
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-25 00:12:18 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/README.md
+++ 프로그래머스/lv1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889#) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 2.96 ms
+메모리: 33.5 MB, 시간: 0.08 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,24 +1,34 @@
+// function solution(N, stages) {
+//     let arr = [];
+//     for(let i=1; i<=N; i++) {
+//         let allPlayer = stages.filter(v=>v>=i).length
+//         let stagePlayer = stages.filter(v=>v===i).length
+//         if(allPlayer===0){
+//             arr.push(0)
+//             continue;
+//         }
+//         let failureRate = (stagePlayer/allPlayer)
+//         arr.push(failureRate)
+//     } 
+//     let stageFail = [...arr]
+//     stageFail.sort((a,b)=>b-a);
+//     const answer = stageFail.map((v,i)=>{
+//         let stage = 0
+//         if(arr.indexOf(v)===arr.lastIndexOf(v)) return arr.indexOf(v)+1
+//         stage = arr.indexOf(v)+1
+//         arr.splice(arr.indexOf(v),1," ")
+//         return stage
+//     })
+//     return answer
+// }
+
 function solution(N, stages) {
-    let arr = [];
-    for(let i=1; i<=N; i++) {
-        let allPlayer = stages.filter(v=>v>=i).length
-        let stagePlayer = stages.filter(v=>v===i).length
-        if(allPlayer===0){
-            arr.push(0)
-            continue;
-        }
-        let failureRate = (stagePlayer/allPlayer)
-        arr.push(failureRate)
-    } 
-    let stageFail = [...arr]
-    stageFail.sort((a,b)=>b-a);
-    console.log(arr,stageFail)
-    const answer = stageFail.map((v,i)=>{
-        let stage = 0
-        if(arr.indexOf(v)===arr.lastIndexOf(v)) return arr.indexOf(v)+1
-        stage = arr.indexOf(v)+1
-        arr.splice(arr.indexOf(v),1," ")
-        return stage
-    })
-    return answer
+    let result = [];
+    for(let i=1; i<=N; i++){
+        let reach = stages.filter((x) => x >= i).length;
+        let curr = stages.filter((x) => x === i).length;
+        result.push([i, curr/reach]);
+    }
+    result.sort((a,b) => b[1] - a[1]);
+    return result.map((x) => x[0]);
 }
