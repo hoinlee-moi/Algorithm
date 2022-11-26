d56c0d759d4d79e8bb115d19deccac4face4512b
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-26 20:51:41 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,10 +1,12 @@
-function solution(a, b, n) {
-    let allBottle = 0
-    while(a<=n) {
-        let changeBottle = Math.floor(n/a)*b
-        let leftBottle = n%a
-        n = changeBottle+leftBottle
-        allBottle += changeBottle
-    }
-    return allBottle;
-}
+// function solution(a, b, n) {
+//     let allBottle = 0
+//     while(a<=n) {
+//         let changeBottle = Math.floor(n/a)*b
+//         let leftBottle = n%a
+//         n = changeBottle+leftBottle
+//         allBottle += changeBottle
+//     }
+//     return allBottle;
+// }
+
+solution = (a, b, n) => Math.floor(Math.max(n - b, 0) / (a - b)) * b
