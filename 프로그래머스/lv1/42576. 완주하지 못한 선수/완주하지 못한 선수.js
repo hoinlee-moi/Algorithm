001a39e07233bb92342f20ad05bbb599393f9914
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-11-30 21:01:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 93.81 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -5,3 +5,27 @@ function solution(participant, completion) {
         if(participant[i]!=completion[i]) return participant[i]
     }
 }
+
+
+/*
+각 배열을 `sort` 로 정돈한 뒤 각 배열의 순서의 값과 일치하지 않으면 리턴하도록 한다.
+이는 배열간의 길이의 차가 1인 것을 근거로 할 수 있다.
+다만 Map을 이용한 풀이가 있었는데 효율이 더 좋게 나왔다.
+function solution(participant, completion) {
+    const map = new Map();
+
+    for(let i = 0; i < participant.length; i++) {
+        let a = participant[i], 
+            b = completion[i];
+
+        map.set(a, (map.get(a) || 0) + 1);
+        map.set(b, (map.get(b) || 0) - 1);
+    }
+
+    for(let [k, v] of map) {
+        if(v > 0) return k;
+    }
+
+    return 'nothing';
+}
+*/
