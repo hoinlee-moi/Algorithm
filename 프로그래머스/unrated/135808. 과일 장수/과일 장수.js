300a0c6ae128814325b32724b217913f096b278a
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-12-05 19:00:24 +0900

    [unrated] Title: 과일 장수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,11 +1,19 @@
+// function solution(k, m, score) {
+//     if(score.length<m) return 0
+//     let money = 0;
+//     score.sort((a,b)=>b-a)
+//     for(let i=0; i<=score.length;i+=m){
+//         if(i%m===0&&i!==0){     
+//             money += score[i-1]*m
+//         }
+//     }
+//     return money;
+// }
 function solution(k, m, score) {
-    if(score.length<m) return 0
-    let money = 0;
-    score.sort((a,b)=>b-a)
-    for(let i=1; i<=score.length;i++){
-        if(i%m===0){     
-            money += score[i-1]*m
-        }
+    let answer = 0;
+    const sortedScore = score.slice().sort((a, b) => a - b).slice(score.length % m);
+    for (let i = 0; i < sortedScore.length; i += m) {
+        answer += sortedScore[i] * m;
     }
-    return money;
+    return answer;
 }
