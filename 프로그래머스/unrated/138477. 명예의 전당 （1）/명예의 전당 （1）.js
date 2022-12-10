23b5204961487058aaf40945f9f217d7b5c0f496
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-12-11 02:41:36 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -15,3 +15,20 @@ function solution(k, score) {
     }
     return answer;
 }
+
+/*
+function solution(k, score) {
+    var temp = [];
+    let answer = [];
+
+    score.forEach(el => {
+        temp.push(el);
+        temp.sort((a, b) => b - a);
+        if(k < temp.length)
+            answer.push(temp[k - 1]);
+        else
+            answer.push(temp[temp.length - 1]);
+    })
+    return answer;
+}
+*/
