714aa7ccf2f9a500a15e6900546b4c1fe1ce55ad
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-12-11 14:25:54 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.76 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -19,3 +19,19 @@ const measure = num => {
     }
     return divisors.size;
 }
+
+// function solution(number, limit, power) {
+//     var answer = 0;
+//     for (let n = 1; n <= number; n++)
+//     {
+//         let count = 0;
+//         for (let j = 1; j * j <= n; j++)
+//         {
+//             if (j * j == n) count++;
+//             else if (n % j == 0) count += 2;
+//         }
+//         if (count > limit) count = power;
+//         answer += count;
+//     }
+//     return answer;
+// }
