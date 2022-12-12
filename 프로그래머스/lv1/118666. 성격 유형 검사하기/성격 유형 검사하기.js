1a3b67c293e1814dedf52a195b7aaf4262ab6236
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-12-13 00:47:24 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.39 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -25,3 +25,31 @@ function solution(survey, choices) {
     }
     return answer
 }
+
+/*
+각 2차원 배열로 만든 후 `Map`을 이용했다. 물론 `Map`을 이용하지 않고 그냥 2차원 배열그대로 해도 되지만 코드가 더 길어질 것 같았다.
+- 먼저 정담 변수 `answer`를 선언하고 각 성격유형의 2차원 배열을 만든 후 `Map`처리한다.
+- `for`문을 돌아 각 번호 선택 때의 점수를 만들어 놓은 `Map`에서 찾아 점수를 더한다.
+- 모든 설문이 끝난 후 계산된 값이 적용된 `Map`객체의 `mbti`를 배열로 바꾼다
+- 다시 `for`문을 돌아 2칸씩잘라내어(처음 배열로 만들 때 2개씩 짝지어 놓았기 때문) `sort`를 이용하여 내림차순 시킨 후 앞에 값의 문자만 가져와 정답 배열에 더한다
+- 이때 둘이 같은 값이면 그냥 둔다.(이유는 처음 객체를 짝을 지어 만들 때 정렬된 값으로 넣어놓았기 때문)
+
+아래는 더 쉽게 풀이 된 정답이다
+
+function solution(survey, choices) {
+    const MBTI = {};
+    const types = ["RT","CF","JM","AN"];
+
+    types.forEach((type) =>
+        type.split('').forEach((char) => MBTI[char] = 0)
+    )
+
+    choices.forEach((choice, index) => {
+        const [disagree, agree] = survey[index];
+
+        MBTI[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
+    });
+
+    return types.map(([a, b]) => MBTI[b] > MBTI[a] ? b : a).join("");
+}
+*/
