e5b7d6a58d6dbb7eac86f501d2b48fd843c1bcbe
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-12-13 13:46:20 +0900

    [unrated] Title: 문자열 나누기, Time: 335.21 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -1,17 +1,31 @@
-function solution(s) {
-    let answer = 0;
-    let standard = s[0]
-    let count = [1,0];
-    for(let i=1;i<=s.length-1;i++){
-        if(s[i]===standard) count[0]++
-        else count[1]++
-        if(count[0]===count[1]){
-            answer++
-            standard = s[i+1]
-            count = [0,0]
-        }
+// function solution(s) {
+//     let answer = 0;
+//     let standard = s[0]
+//     let count = [1,0];
+//     for(let i=1;i<=s.length-1;i++){
+//         if(s[i]===standard) count[0]++
+//         else count[1]++
+//         if(count[0]===count[1]){
+//             answer++
+//             standard = s[i+1]
+//             count = [0,0]
+//         }
         
+//     }
+//     if(count[0]!==0||count[1]!==0) answer++
+//     return answer;
+// }
+
+function solution(s, count=0) {
+    if(!s) return count
+    let [first, ...rest] = s.split("")
+    let countSame = 1
+    let countInSame = 0
+    let i=0
+    for(; i<rest.length; i++){
+        if(rest[i] === first) countSame++
+        else countInSame++
+        if(countSame === countInSame) break
     }
-    if(count[0]!==0||count[1]!==0) answer++
-    return answer;
+    return solution(rest.slice(i+1).join(""), count+1)
 }
