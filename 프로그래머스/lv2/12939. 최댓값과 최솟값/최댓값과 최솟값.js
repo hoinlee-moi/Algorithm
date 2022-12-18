022d13e5ce011431e6b64ec99d6db08be94314e1
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-12-18 15:14:34 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -2,3 +2,5 @@ function solution(s) {
     const arr = s.split(" ").map(v=>+v)
     return `${Math.min(...arr)} ${Math.max(...arr)}`;
 }
+
+// Math가 문자열도 가능하다는 사실을 몰라서 `map`반복문이 한 번 들어갔는데 문자열도 자동 형변환 돼서 가능하다면 굳이 `map`을 쓸 필요가 없을 것 같다.
