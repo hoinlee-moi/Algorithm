0bee4d33388ff6e2c3af672d109ca87be643cec8
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-12-26 17:13:25 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,3 +1,33 @@
+function solution(brown, yellow) {
+    let area = measure(brown+yellow);
+    let wh = [];
+    for(let i=0; i<=area.length-1;i++){
+        wh.push(area[i])
+        if(wh.length===2){
+            if((wh[0]-2)*(wh[1]-2)===yellow) break;
+            else {
+                wh.pop()
+                wh.pop()
+            }
+        } 
+    }
+    if(wh.length===1) return [wh[0],wh[0]]
+    return [wh[1],wh[0]];
+}
+
+const measure = num => {
+    const divisors = [];
+    for(let i = 1 ; i <= Math.sqrt(num) ; i++){
+        if(num % i === 0) {
+            divisors.push(i);
+            if(num / i != i) divisors.push(num / i);
+        }
+    }
+    return divisors;
+}
+
+/*
+많이 특이한 방법으로 푼 것 같다.
 function solution(brown, red) {
     var answer = [];
     for (var i = 3; i <= (brown+red)/i; i++) {
@@ -9,3 +39,4 @@ function solution(brown, red) {
 
     return [x,i];
 }
+*/
