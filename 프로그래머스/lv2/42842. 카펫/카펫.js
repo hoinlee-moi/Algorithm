26e46eb09f7f6bba002c37e73dd7d4d42d7b299a
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-12-26 17:12:22 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,11 +1,11 @@
-function solution(brown, yellow) {
-    const x = (brown - 12) * 0.5;
-    const y = yellow - brown + 8;
-    const i = ( 8 + x + Math.sqrt( Math.pow(x, 2) - (4 * y) ) ) * 0.5;
-    const j = ( 8 + x - Math.sqrt( Math.pow(x, 2) - (4 * y) ) ) * 0.5;
-    console.log(i)
+function solution(brown, red) {
+    var answer = [];
+    for (var i = 3; i <= (brown+red)/i; i++) {
+        var x = Math.floor((brown+red)/i);
+        if( (x-2)*(i-2)=== red) {
+            break;
+        }
+    }
 
-
-    var answer = [i, j];
-    return answer;
+    return [x,i];
 }
