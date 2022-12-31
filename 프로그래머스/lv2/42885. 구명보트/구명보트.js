e15e31428056a384db39a86b9ab8cabf9754a61b
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-12-31 19:56:10 +0900

    [level 2] Title: 구명보트, Time: 33.12 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,11 +1,18 @@
 function solution(people, limit) {
     let answer = 0;
     people.sort((a,b)=>b-a)
-    for(let i=0 , j=people.length-1; i<=j;i++){
-        if(people[i]+people[j]<=limit){
-            j--
+    for(let i=0; i<=people.length-1;i++){
+        if(people[i]+people[people.length-1]<=limit){
+            people.pop()
         }
         answer++
     }
     return answer;
 }
+
+/*
+- 탐욕법을 쓰기 때문에 `limit`에 가장 가까운 큰 값을 먼저 뺀뒤 남은 숫자가 가장 작은 값에 들어갈 수 있는지를 체크한다
+- 그래서 `sort`로 먼저 내림차순을 한 뒤 반복문을 돌아 가장 큰값 (`i`번째)와 배열의 마지막값을 합해 `limit`에 걸리는지 체크하고 문제 없다면 `pop()`을 이용해 맨 마지막 값을 빼준다
+- 이는 반복문을 통해 큰 숫자는 차례대로 가지만 맨 마지막에 있는 가장 작은 값은 그대로 남아있어 반복을 돌다 한번 더 체크 될 수 있기 때문에 `pop()`을 통해 아예 제거 한다.
+- 그러면 자연스레 총 길이도 줄어들고 구명보트 개수의 최솟값이 도출된다.
+*/
