db33f88810fd7b44b9650b64b34214aa6587cf5a
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2022-12-31 19:51:06 +0900

    [level 2] Title: 구명보트, Time: 11.22 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,9 +1,9 @@
 function solution(people, limit) {
     let answer = 0;
     people.sort((a,b)=>b-a)
-    for(let i=0; i<=people.length-1;i++){
-        if(people[i]+people[people.length-1]<=limit){
-            people.pop()
+    for(let i=0 , j=people.length-1; i<=j;i++){
+        if(people[i]+people[j]<=limit){
+            j--
         }
         answer++
     }
