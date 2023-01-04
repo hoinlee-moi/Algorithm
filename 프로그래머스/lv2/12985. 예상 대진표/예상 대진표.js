bf96c60c6562cf606166f3fb2cbd4636526fff48
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2023-01-04 14:55:15 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,13 +1,25 @@
-function solution(n,a,b){
-    let answer = 0
-    let A = Math.min(a,b)
-    let B = Math.max(a,b)
-    while(n%2===0) {
-        answer++
-        if(A+1===B && B%2===0) break;
-        A = Math.ceil(A/2)
-        B = Math.ceil(B/2)
-        n = n/2
+// function solution(n,a,b){
+//     let answer = 0
+//     let A = Math.min(a,b)
+//     let B = Math.max(a,b)
+//     while(n%2===0) {
+//         answer++
+//         if(A+1===B && B%2===0) break;
+//         A = Math.ceil(A/2)
+//         B = Math.ceil(B/2)
+//         n = n/2
+//     }
+//     return answer;
+// }
+
+function solution(n,a,b)
+{
+    let answer = 0;
+    while(a !== b) {
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        answer++;
     }
+
     return answer;
 }
