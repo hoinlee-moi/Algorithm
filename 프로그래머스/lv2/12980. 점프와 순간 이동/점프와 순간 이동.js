ee8f59d258f065d1b0a05f7eac5ac42f1b112f3f
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2023-01-09 11:46:20 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,12 +1,5 @@
 function solution(n){
-    let ans = 0;
-    while(true){
-        if(n%2!==0) {
-            n -= 1
-            ans++
-        }
-        n /= 2
-        if(n <=0) break;
-    }
-    return ans;
+    if(n === 1) return 1;
+    const nArr = Array.from(n.toString(2));
+    return nArr.reduce((a,b)=>(+a)+(+b));
 }
