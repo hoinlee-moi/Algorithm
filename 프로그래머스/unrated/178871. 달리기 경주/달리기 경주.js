19098c89772b19285887f37884b0e4d1057f6196
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2023-06-16 16:01:05 +0900

    [unrated] Title: 달리기 경주, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,19 +1,21 @@
 function solution(players, callings) {
-    let idx;
-    let name1;
-    let name2;
-    const idxList = {}
-
-    players.forEach((name,index)=>idxList[name]=index)
-    for(let call of callings){
-        idx = idxList[call]
-        name1 = players[idx]
-        name2 = players[idx-1]
-        idxList[call]-=1
-        idxList[name2]+=1
-        players[idx] = name2
-        players[idx-1] = name1
-    }    
-
-    return players;
+    let rankMap = new Map()
+    let playerMap = new Map()
+    let result = []
+    players.map((v,i)=>{
+        rankMap.set(i+1,v)
+        playerMap.set(v,i+1)
+    })
+    callings.map(v=>{
+        const rank = playerMap.get(v)
+        const front = rankMap.get(rank-1)
+        playerMap.set(v,rank-1)
+        playerMap.set(front,rank)
+        rankMap.set(rank-1,v)
+        rankMap.set(rank,front)
+    })
+    for(let value of rankMap.values()){
+        result.push(value)
+    }
+    return result;
 }
