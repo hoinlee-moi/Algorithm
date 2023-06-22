8c3c28bbcfa08882316eb8882b42c988886dbc25
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2023-06-22 14:35:59 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.10 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,15 +1,7 @@
 function solution(n, left, right) {
-    let answer = [];
-    let start = [Math.floor(left/n)+1,left%n]
-    let end = [Math.floor(right/n)+1,right%n]
-    for(let i=start[0];i<=end[0];i++) {
-        let idx =1;
-        if(i===start[0]) idx = start[1]+1
-        if(i===end[0]) n = end[1]+1
-        for(let j=idx;j<=n;j++) {
-            let num = i>=j?i:j
-            answer.push(num)
-        }
+    var answer = [];
+    for (let i = left; i <= right; i++) {
+        answer.push(Math.max(i % n, parseInt(i / n)) + 1)
     }
     return answer;
 }
