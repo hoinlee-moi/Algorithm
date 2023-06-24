c492534f6d2402297553cc29b089b4a94bcb0f16
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2023-06-24 15:01:19 +0900

    [level 2] Title: [1차] 캐시, Time: 6.28 ms, Memory: 34.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -0,0 +1,33 @@
+function solution(cacheSize, cities) {
+    let answer = 0;
+    let obj = {}
+    for(let i=0; i<=cities.length-1;i++) {
+        if(cacheSize===0) {
+            answer+=5
+            continue;
+        }
+        let city = cities[i].toLowerCase()
+        let key = getKeyByValue(obj,city)
+        if(key!==undefined){
+            answer += 1
+            delete(obj[key])
+            obj[i] = city
+            continue;
+        }
+        if(Object.keys(obj).length===cacheSize) {
+            let idx = Math.min(...Object.keys(obj)).toString()
+            delete(obj[idx])
+            obj[i] = city
+            answer += 5
+            continue;
+        }
+        obj[i] = city
+        answer +=5
+    }
+    
+    return answer;
+}
+
+function getKeyByValue(obj, value) {
+  return Object.keys(obj).find(key => obj[key] === value);
+}
