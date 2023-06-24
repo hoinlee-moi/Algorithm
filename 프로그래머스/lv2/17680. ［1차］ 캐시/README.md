649875c74123c2335c468c8a8b539eaf45f75a13
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2023-06-24 15:02:45 +0900

    [level 2] Title: [1차] 캐시, Time: 7.05 ms, Memory: 34.8 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 0.78 ms
+메모리: 34.8 MB, 시간: 7.05 ms
 
 ### 구분
 
--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,26 +1,33 @@
 function solution(cacheSize, cities) {
-    const MISS = 5, HIT = 1;
-
-    if (cacheSize === 0) return MISS * cities.length;
-
-    let answer = 0,
-        cache = [];
-
-    cities.forEach(city => {
-        city = city.toUpperCase();
-
-        let idx = cache.indexOf(city);
-
-        if (idx > -1) {
-            cache.splice(idx, 1);
-            answer += HIT;
-        } else {
-            if (cache.length >= cacheSize) cache.shift();
-            answer += MISS;
+    let answer = 0;
+    let obj = {}
+    for(let i=0; i<=cities.length-1;i++) {
+        if(cacheSize===0) {
+            answer+=5
+            continue;
         }
-
-        cache.push(city);
-    });
+        let city = cities[i].toLowerCase()
+        let key = getKeyByValue(obj,city)
+        if(key!==undefined){
+            answer += 1
+            delete(obj[key])
+            obj[i] = city
+            continue;
+        }
+        if(Object.keys(obj).length===cacheSize) {
+            let idx = Math.min(...Object.keys(obj)).toString()
+            delete(obj[idx])
+            obj[i] = city
+            answer += 5
+            continue;
+        }
+        obj[i] = city
+        answer +=5
+    }
 
     return answer;
+}
+
+function getKeyByValue(obj, value) {
+  return Object.keys(obj).find(key => obj[key] === value);
 }
