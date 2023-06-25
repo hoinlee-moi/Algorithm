54e0904f2c1c7c5b1fa83f58a65a1100e5ca5eac
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2023-06-25 11:16:54 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.js
+++ 프로그래머스/lv2/42578. 의상/의상.js
@@ -1,6 +1,14 @@
 function solution(clothes) {
-    return Object.values(clothes.reduce((obj, t)=> {
-        obj[t[1]] = obj[t[1]] ? obj[t[1]] + 1 : 1;
-        return obj;
-    } , {})).reduce((a,b)=> a*(b+1), 1)-1;    
+    let answer =1;
+    let clothe = {}
+    clothes.forEach(v=>{
+        clothe[v[1]] = clothe[v[1]]?clothe[v[1]]+1:1
+    })
+    let arr = Object.values(clothe)
+    if(arr.length===1) return clothes.length
+    let r = arr
+    for(let i=0; i<arr.length; i++) {
+        answer *= arr[i]+1
+    }  
+    return answer-1;
 }
