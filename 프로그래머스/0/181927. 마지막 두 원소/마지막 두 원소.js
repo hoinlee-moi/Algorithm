e101002210fe451a29054f1f947acc112d67512f
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2024-01-29 13:03:35 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -3,3 +3,11 @@ function solution(num_list) {
     const lastBefore = num_list.at(-2)
     return  last>lastBefore?num_list.push(last-lastBefore):num_list.push(last*2),num_list
 }
+/*
+function solution(num_list) {
+    const [a, b] = [...num_list].reverse();
+    return [...num_list, a > b ? (a-b):a*2];
+}
+숏코딩이긴 하나 const[] 선언으로 배열 하나 만드는 메모리가 사용되고 또 reverse를 통해 메모리가 뒤집어 진다
+추가적으로 return 할때 또 배열을 만듬으로써 성능적으론 좋지 않다
+*/
