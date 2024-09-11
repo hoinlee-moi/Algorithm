cd7104db8a5382aedc658e277280a311c6a55d5a
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2024-09-11 16:11:28 +0900

    [level 1] Title: 실패율, Time: 6160.16 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,28 +1,4 @@
 function solution(N, stages) {
-    let arr = [];
-    for(let i=1; i<=N; i++) {
-        let allPlayer = stages.filter(v=>v>=i).length
-        let stagePlayer = stages.filter(v=>v===i).length
-        if(allPlayer===0){
-            arr.push(0)
-            continue;
-        }
-        let failureRate = (stagePlayer/allPlayer)
-        arr.push(failureRate)
-    } 
-    let stageFail = [...arr]
-    stageFail.sort((a,b)=>b-a);
-    const answer = stageFail.map((v,i)=>{
-        let stage = 0
-        if(arr.indexOf(v)===arr.lastIndexOf(v)) return arr.indexOf(v)+1
-        stage = arr.indexOf(v)+1
-        arr.splice(arr.indexOf(v),1," ")
-        return stage
-    })
-    return answer
-}
-/*
-function solution(N, stages) {
     let result = [];
     for(let i=1; i<=N; i++){
         let reach = stages.filter((x) => x >= i).length;
@@ -32,6 +8,7 @@ function solution(N, stages) {
     result.sort((a,b) => b[1] - a[1]);
     return result.map((x) => x[0]);
 }
+/*
  하드코딩에 가깝게 햇는데 이사람의 풀이는
  reach 는 스테이지에 도달한 사람의 수 이고 curr은 도전중인 사람이다.
  그래서 result에 푸쉬 하는데 형태는 [스테이지, 실패율]로 들어가게 된다.
