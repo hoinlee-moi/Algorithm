4b05faaf350d9fb9bdb6515b8d69616783a0ea5c
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2024-09-23 03:45:58 +0900

    [level 2] Title: 괄호 회전하기, Time: 12.36 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,39 +1,38 @@
-
-function solution(s) { 
-    let count = 0
-    for(let i=0; i<=s.length-1;i++) {
-        let stack = []
-        let str = ""
-        str += s.slice(i)
-        if(i>0) str += s.slice(0,i)
-        stack.push(str[0])
-        for(let j=1; j<=str.length-1;j++){
-            stack.push(str[j])
-            if(str[j]===")"){
-                if(stack[stack.length-2]!=="(") break;
-                else {
-                    stack.pop()
-                    stack.pop()
-                }
+function solution(s) {
+    let count = 0;
+    const len = s.length
+    for(let i=0; i<len; i+=1){
+        const stack = [];
+        let suc = true;
+        for(let j=0; j<len; j+=1) {
+            const c = s[(i+j)%len];
+            if(c ==='(' || c === '{'|| c === '[' ) {
+                stack.push(c)
+                continue;
             }
-            if(str[j]==="}"){
-                if(stack[stack.length-2]!=="{") break;
-                else {
-                    stack.pop()
-                    stack.pop()
-                }
+            else {
+                if(!stack.length) {
+                    suc = false
+                    break
+                };
             }
-            if(str[j]==="]"){
-                if(stack[stack.length-2]!=="[") break;
-                else {
-                    stack.pop()
-                    stack.pop()
-                }
+            
+            const top = stack[stack.length-1]
+            
+            switch(c){
+                case ')':
+                    top==='(' && stack.pop();
+                    break;
+                case ']':
+                    top === '[' && stack.pop();
+                    break;
+                case '}':
+                    top === '{' && stack.pop()
+                    break;
+                default:suc = false
             }
         }
-        if(stack.length===0) {
-            count++
-        }
+        if(suc && !stack.length) count +=1;
     }
-    return count;
+    return count
 }
