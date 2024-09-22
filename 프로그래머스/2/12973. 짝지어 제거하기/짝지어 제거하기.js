e656d6b58773c7d86bd2f6839326bfdb1ebc6352
ho_in <103922658+hoinlee-moi@users.noreply.github.com>
2024-09-23 04:00:10 +0900

    [level 2] Title: 짝지어 제거하기, Time: 51.27 ms, Memory: 46.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -2,7 +2,7 @@ function solution(s){
     if(s.length % 2 != 0) return 0;
     const stack = []
     for(let i=0; i<=s.length-1; i+=1){
-        if(stack.length && stack.at(-1) === s[i]) stack.pop()
+        if(stack.length && stack[stack.length-1] === s[i]) stack.pop()
         else stack.push(s[i])
     }
     return stack.length ? 0 : 1;
